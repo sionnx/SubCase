8b07ad65918ca73344d8f703c5b00620eff2060f
Sion <i@reao.io>
2026-09-07 18:20:44 +0800

    chore: bump version to 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val refId = repo.refDatabase.exactRef("refs/remotes/origin/main").objectId!!
 val commitCount = Git(repo).log().add(refId).call().count()
 
 val verCode by extra(commitCount)
-val verName by extra("1.0.0")
+val verName by extra("1.1.0")
 
 val androidTargetSdkVersion by extra(34)
 val androidCompileSdkVersion by extra(34)
